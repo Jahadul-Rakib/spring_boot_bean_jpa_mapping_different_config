ef23094edebfd13d3e3140370e2457584f6d24b9
rakib <jhadul.islam@itconquest.com>
2021-02-17 17:41:42 +0600

    add

--- src/main/java/com/bean_scope_and_mapping/configuration/SchedulerConfig.java
+++ src/main/java/com/bean_scope_and_mapping/configuration/SchedulerConfig.java
@@ -2,13 +2,14 @@ package com.bean_scope_and_mapping.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 @EnableScheduling
 public class SchedulerConfig {
 
     //Scheduler will run every 30 second later
-    //@Scheduled(fixedDelay = 30000)
+    @Scheduled(fixedDelay = 30000)
     public void print(){
         System.out.println("Calling from Scheduler.");
     }
